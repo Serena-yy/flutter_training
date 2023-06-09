617166b826fc8d3e0db2b7e5c84095351684f95f
serena <serena6105@gmail.com>
2023-06-10 03:24:14 +0900

    06Jun练习

---
+++ 06JUN.dart
@@ -0,0 +1,89 @@
+import 'package;flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'simple login',
+      home: AppPage(),
+    ));
+
+class AppPage extends StatelessWidget {
+  GlobalKey<FormState> _loginKey = GlobalKey();
+  String _username;
+  String _password;
+
+  void _login() {
+    var loginForm = _loginKey.currentState;
+    if (login.form.validate()) {
+      loginForm.save();
+      print('user name = $_username --- password = $_password');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple login'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(16.0),
+        child: Center(
+          child: Form(
+            key: _loginKey,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: '请输入登录用户名',
+                        hintText: '请输入登录用户',
+                        border: OutlineInputBorder()),
+                    onSaved: (val) {
+                      _username = val;
+                    },
+                    onFieldSubmitted: (val) {},
+                    validator: (val) {
+                      if (val.length == 0) return '用户名不能为空';
+                    },
+                  ),
+                  SizeBox(
+                    height: 30.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: '请输入用户密码',
+                        hinttext: '请输入用户密码',
+                        border: OutlineInputBorder()),
+                    onSaved: (val) {
+                      _password = val;
+                    },
+                    onFieldSubmitted: (val) {},
+                    validator: (val) {
+                      if (val.length < 3) return '密码不能小于3个字符';
+                      if (val.length > 6)
+                        return '密码不能大于6个字符';
+                      else
+                        return null;
+                    },
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30.0),
+                    child: SizedBox(
+                      width: 400.0,
+                      height: 45.0,
+                      child: RaisedButtion(
+                        onPressed: () {},
+                        child: Text('登录',
+                            style:
+                                TextStyle(color: Colors.block, fontSize: 20.0)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
